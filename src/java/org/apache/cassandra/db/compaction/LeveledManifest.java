98b0032050fa8f3b0ddcdcf078b4228b6d82bd56
vusal-ahmadoglu <vusal.ahmadoglu@gmail.com>
2017-09-24 02:19:23 +0200

    Reverted changes back to see that tests are ok ;

--- src/java/org/apache/cassandra/db/compaction/LeveledManifest.java
+++ src/java/org/apache/cassandra/db/compaction/LeveledManifest.java
@@ -348,7 +348,7 @@ public class LeveledManifest
 
         // Let's check that L0 is far enough behind to warrant STCS.
         // If it is, it will be used before proceeding any of higher level
-        CompactionCandidate l0Compaction = getSTCSInL0CompactionCandidate();
+//        CompactionCandidate l0Compaction = getSTCSInL0CompactionCandidate();
 
         for (int i = generations.length - 1; i > 0; i--)
         {
@@ -364,6 +364,7 @@ public class LeveledManifest
             if (score > 1.001)
             {
                 // before proceeding with a higher level, let's see if L0 is far enough behind to warrant STCS
+                CompactionCandidate l0Compaction = getSTCSInL0CompactionCandidate();
                 if (l0Compaction != null)
                     return l0Compaction;
 
@@ -393,7 +394,7 @@ public class LeveledManifest
             // Since we don't have any other compactions to do, see if there is a STCS compaction to perform in L0; if
             // there is a long running compaction, we want to make sure that we continue to keep the number of SSTables
             // small in L0.
-            return l0Compaction;
+            return getSTCSInL0CompactionCandidate();
         }
         return new CompactionCandidate(candidates, getNextLevel(candidates), maxSSTableSizeInBytes);
     }
